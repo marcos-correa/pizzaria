f5c72cfe744043d774c079545dc8c727787b6639
marcos <marcoscorreats@gmail.com>
2022-04-14 03:07:24 -0300

    mudança do verbo list para POST com verificação de token

--- src/api/list.php
+++ src/api/list.php
@@ -1,5 +1,19 @@
 <?php
     $query = require ('connect.php'); //conexao banco
-    $user = $query ->selectAll($table);
+    require('jwt.php');
+
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    if (isset($request) && !empty($request)) {
+      $token = $request->data->token;
+      if(verificaTokenBody($token)){
+        $user = $query ->selectAll($table);
+        echo json_encode(['data'=>$user]);
+      }
+      else{
+        $code = 404;
+        $reason = 'Acesso nao autorizado. Falha na autenticacao';
+        header("HTTP/1.0 $code $reason");
+      }
+    }
     
-    echo json_encode(['data'=>$user]);
