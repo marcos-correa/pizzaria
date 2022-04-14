6108b594890c86b0afe537ce0f7ba9f9584eaddb
marcos <marcoscorreats@gmail.com>
2022-04-14 03:06:40 -0300

    backend - novas funçẽos JWT

--- src/api/jwt.php
+++ src/api/jwt.php
@@ -28,6 +28,46 @@ function gerarToken($email, $senha){
 }
 
 
+function verificaToken($http_header)
+{
+  if (isset($http_header['Authorization']) && $http_header['Authorization'] != null) {
+      $bearer = explode (' ', $http_header['Authorization']);
+      $token = explode('.', $bearer[1]);
+      $header = $token[0];
+      $payload = $token[1];
+      $sign = $token[2];
+
+      $valid = hash_hmac('sha256', $header . "." . $payload, 'trabalho_web_servidor', true);
+      $valid = base64Erlencode($valid);
+
+      if ($sign === $valid) {
+        return true;
+      }
+  }
+
+  return false;
+}
+
+function verificaTokenBody($bearer_)
+{
+  if (isset($bearer_) && $bearer_ != null) {
+      $bearer = explode (' ', $bearer_);
+      $token = explode('.', $bearer[1]);
+      $header = $token[0];
+      $payload = $token[1];
+      $sign = $token[2];
+
+      $valid = hash_hmac('sha256', $header . "." . $payload, 'trabalho_web_servidor', true);
+      $valid = base64Erlencode($valid);
+
+      if ($sign === $valid) {
+        return true;
+      }
+  }
+
+  return false;
+}
+
 ?>
 
 
