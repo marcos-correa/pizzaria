bfe69a770dd175eadd9ed3315edca1f81fa3f109
marcos <marcoscorreats@gmail.com>
2022-05-22 21:05:54 -0300

    atualizando o proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROX_CONFIG = [
   {
     context:['/api'],
-    target: 'http://localhost:80/',
+    target: 'http://localhost:8080/',
     secure: false,
     changeOrigin:true,
     logLevel: 'debug'
