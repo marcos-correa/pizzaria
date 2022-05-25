e20775594a580a9a0fe8232e027ee6bb3c045d1c
marcos <marcoscorreats@gmail.com>
2022-05-25 19:18:43 -0300

    Erro do token

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -38,24 +38,16 @@ export class AuthenticationService {
   }
 
   getToken(){
-    return  "Bearer "+ JSON.parse(window.localStorage.getItem('token')?.toString());
-
-    // let token = window.localStorage.getItem('token')?.toString()
-    // // let auth = "Bearer "
-    // let data = {
-    //   token:"Bearer "+JSON.parse(token)
-    // }
-    // if(token){
-    //   // auth += JSON.parse(token)
-    //   data.token += JSON.parse(token)
-    // }
+    if(window.localStorage.getItem('token')?.toString()){
+      return  "Bearer "+ JSON.parse(window.localStorage.getItem('token')?.toString());
+    }else{
+      return ""
+    }
   }
 
   verificarToken(token:any){
     let token_ = token;
     
     let {email} = this.usuario
-
-    // return this.http.get(`/api/VerificaToken?token=${token_},email=${email}`)
   }
 }
